88ef86d190476def7630f44bfbf76a43a9bd5ba1
crilillo14 <93829520+crilillo14@users.noreply.github.com>
2025-04-07 12:27:41 -0400

    boat header file completed

--- include/boat.h
+++ include/boat.h
@@ -1,45 +1,72 @@
-/*                      -- boat.h -- 
- * HEADER FILE - 
- * declares 
- * 
+/*                      -- boat.h --
+ * HEADER FILE -
+ * declares
+ *
  */
 
+#ifndef BOAT_H
+#define BOAT_H
 
+#include <cstdint>
 #include <stdint.h>
 
-// CONSTANTS --------------------------------------------------------------------------------------
+// CONSTANTS
+// --------------------------------------------------------------------------------------
 
 #define MAX_BOAT_NAME_LENGTH 128 // +1 for null terminator: s[127] + \0
 #define MARINA_MAX_BOAT_CAPACITY 120
+// ok, so there's no constraint for the licence tag length, but mallocing for
+// *char instead of some char[] with fixed lenght is goign to be SO MUCH easier.
+// from the given sample output, it seems that it's at most 6 chars, +1 for null
+// terminator, so to keep it safe, im having it be 10. 9 chars, one null
+// terminator
+#define MAX_LICENSE_TAG_LENGTH 10
 
-// TYPEDEFS ---------------------------------------------------------------------------------------
+// TYPEDEFS
+// ---------------------------------------------------------------------------------------
 
-typedef enum {
-    slip,
-    land,
-    trailor,
-    storage,
-    no_place
-} PlaceType;
+typedef enum { slip, land, trailor, storage, no_place } PlaceType;
+
+/* For PlaceInfo:
+
+Slip - the slip number 1-85 -> 7 bits, make it uint8 too
+Land - the bay letter, A-Z -> char
+Trailor - the trailor license tag -> no restriction imposed on length or format,
+*char Storage - the storage space number 1-50 -> 6 bits, use uint8_t too.
+
+*/
 
 typedef union {
-    int slipNumber;         // 1-85
-    char bayLetter;         // A-Z
-    char licenseTag[MAX_LICENSE_TAG_LENGTH];
-    int storageNumber;      // 1-50
+  uint8_t slipNumber; // 1-85
+  char bayLetter;     // A-Z
+  char licenseTag[MAX_LICENSE_TAG_LENGTH];
+  uint8_t storageNumber; // 1-50
 } PlaceInfo;
 
-
 typedef struct {
-  char name[MAX_BOAT_NAME_LENGTH]; 
-  uint8_t length;           // uint8_t spans the range of valid boat lengths, saves memory space.
-  PlaceType place; 
-  double moneyOwed;         // double cus it can get expensive to own a boat, also a real number.
-  PlaceInfo extraInfo;      // depending on the PlaceType.
+  char name[MAX_BOAT_NAME_LENGTH];
+  uint8_t length; // uint8_t spans the range of valid boat lengths, saves memory
+                  // space.
+  PlaceType place;
+  double moneyOwed;    // double cus it can get expensive to own a boat, also a
+                       // real number.
+  PlaceInfo extraInfo; // depending on the PlaceType.
 } Boat;
 
 // to be used in main.c
 extern Boat *boatInventory[MARINA_MAX_BOAT_CAPACITY];
 extern int boatCount;
 
+// Function declarations
+PlaceType StringToPlaceType(char *PlaceString);
+char *PlaceToString(PlaceType Place);
+void initializeBoatInventory();
+int addBoat(const char *csvLine);
+int removeBoat(const char *name);
+int findBoatByName(const char *name);
+void makePayment(const char *name, double payment);
+void updateMonthlyCharges();
+void sortInventory();
+void freeBoatInventory();
 
+#endif // BOAT_H
--- src/boat.c
+++ src/boat.c
@@ -1,6 +1,6 @@
 /*
 
-Definition of a boat (as relevant to this program): 
+Definition of a boat (as relevant to this program):
 
  - The name of the boat (up to 127 characters long, not containing a comma)
  - The length in feet - up to 100' (hint: choose a suitable data type)
@@ -23,81 +23,55 @@ Depending on the type, extra relevant information (use a union):
  - The amount of money owed to the marina
  */
 
-
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
 #include "../include/boat.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-// CONSTANTS --------------------------------------------------------------------------------------
+// STRING UTILITY
+// --------------------------------------------------------------------------------
 
-#define MAX_BOAT_NAME_LENGTH 128 // +1 for null terminator: s[127] + \0
-#define MARINA_MAX_BOAT_CAPACITY 120
-
-// TYPEDEFS ---------------------------------------------------------------------------------------
-
-typedef enum {
-    slip,
-    land,
-    trailor,
-    storage,
-    no_place
-} PlaceType;
-
-
-typedef struct {
-  char name[MAX_BOAT_NAME_LENGTH]; 
-  uint8_t length;   // uint8_t spans the range of valid boat lengths, saves memory space.
-  PlaceType place; 
-  double moneyOwed; // double cus it can get expensive to own a boat, also a real number.
-} Boat;
-
-// to be used in main.c
-extern Boat *boatInventory[MARINA_MAX_BOAT_CAPACITY];
-extern int boatCount;
-
-// STRING UTILITY --------------------------------------------------------------------------------
-
-// { given by professor } 
+// { given by professor }
 //----Convert a string to a place
-PlaceType StringToPlaceType(char * PlaceString) {
+PlaceType StringToPlaceType(char *PlaceString) {
 
-    if (!strcasecmp(PlaceString,"slip")) {
-        return(slip);
-    }
-    if (!strcasecmp(PlaceString,"land")) {
-        return(land);
-    }
-    if (!strcasecmp(PlaceString,"trailor")) {
-        return(trailor);
-    }
-    if (!strcasecmp(PlaceString,"storage")) {
-        return(storage);
-    }
-    return(no_place);
+  if (!strcasecmp(PlaceString, "slip")) {
+    return (slip);
+  }
+  if (!strcasecmp(PlaceString, "land")) {
+    return (land);
+  }
+  if (!strcasecmp(PlaceString, "trailor")) {
+    return (trailor);
+  }
+  if (!strcasecmp(PlaceString, "storage")) {
+    return (storage);
+  }
+  return (no_place);
 }
 //-------------------------------------------------------------------------------------------------
 
 // { given by professor }
 //----Convert a place to a string
-char * PlaceToString(PlaceType Place) {
+char *PlaceToString(PlaceType Place) {
 
-    switch (Place) {
-        case slip:
-            return("slip");
-        case land:
-            return("land");
-        case trailor:
-            return("trailor");
-        case storage:
-            return("storage");
-        case no_place:
-            return("no_place");
-        default:
-            printf("How the faaark did I get here?\n");
-            exit(EXIT_FAILURE);
-            break;
-    }
+  switch (Place) {
+  case slip:
+    return ("slip");
+  case land:
+    return ("land");
+  case trailor:
+    return ("trailor");
+  case storage:
+    return ("storage");
+  case no_place:
+    return ("no_place");
+  default:
+    printf("How the faaark did I get here?\n");
+    exit(EXIT_FAILURE);
+    break;
+  }
 }
 
-// CORE CLASS FUNCTIONALITY ------------------------------------------------------------------------
+// CORE CLASS FUNCTIONALITY
+// ------------------------------------------------------------------------
